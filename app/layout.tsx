744c6c4a36229bd34e71e9c60d523cf08010a03f
salathleizhang <79990049+salathleizhang@users.noreply.github.com>
2024-07-27 18:03:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,8 @@ const meta = {
 export const metadata: Metadata = {
   ...meta,
   title: {
-    default: 'answer website',
-    template: `%s - answer website`,
+    default: 'InnoChat',
+    template: `%s - InnoChat`,
   },
   icons: {
     icon: '/favicon.ico',
