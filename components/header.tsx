01bb8d1fa88078c105412f0e5ffb2183b6b8f161
salathleizhang <79990049+salathleizhang@users.noreply.github.com>
2024-08-31 16:36:42 +0800

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -23,7 +23,7 @@ export function Header() {
         </div>
         <span className="inline-flex items-center home-links whitespace-nowrap">
           <a href="https://google.com" rel="noopener" target="_blank">
-            <span className="block sm:inline text-lg sm:text-xl lg:text-2xl font-semibold dark:text-white text-black">Inno <span className="linear-wipe">Chat</span></span>
+            <span className="block sm:inline text-lg sm:text-xl lg:text-2xl font-semibold dark:text-white text-black">Inno <span className="linear-wipe">Search</span></span>
           </a>
         </span>
       </header>
