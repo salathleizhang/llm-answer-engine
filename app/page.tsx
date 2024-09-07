63cbc6244aa83fbf63a9468c4ec107c7fbfdcaab
salathleizhang <79990049+salathleizhang@users.noreply.github.com>
2024-09-07 18:29:46 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -368,7 +368,7 @@ export default function Page() {
       <div className={`px-2 fixed inset-x-0 bottom-0 w-full bg-gradient-to-b duration-300 ease-in-out animate-in dark:from-gray-900/10 dark:from-10% peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]] mb-4 bring-to-front`}>
         <div className="mx-auto max-w-xl sm:px-4 ">
           {messages.length === 0 && !inputValue && (
-            <InitialQueries questions={['When did Daft Punk release Da Funk?', 'How is Apple\'s stock doing these days?', 'Where can I get the best bagel in NYC?', 'I want to buy a mens patagonia vest']} handleFollowUpClick={handleFollowUpClick} />
+            <InitialQueries questions={['InnoSphere株式会社について教えてください', '東京都内でおしゃれなカフェを教えてください', '最近のITニュースを教えてください', 'AIに関する論文を探してください']} handleFollowUpClick={handleFollowUpClick} />
           )}
           {mentionQuery && (
             <div className="">
